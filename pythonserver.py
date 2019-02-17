8eaf906a5f30365bd32395c579ec84c50a608a0d
shashank2000 <46056525+shashank2000@users.noreply.github.com>
2019-02-17 02:39:42 -0800

    Update pythonserver.py
    Might not work

--- pythonserver.py
+++ pythonserver.py
@@ -1,37 +1,39 @@
-from flask import Flask
+from flask import Flask                                        
 import requests
 import json
 from textblob import TextBlob
 from bs4 import BeautifulSoup
 import re
-import json
+
 app = Flask(__name__)
 
 RESULTS = 'results'
 BILLS = 'bills'
 DESCRIPTION = 'description'
 GOOGLE_API_KEY = 'AIzaSyA6msAMADzxXZxANboADutjj6RkQSPAyuA'
+
+
 @app.route("/")
 def hello():
     header_data = {'X-API-Key' : 'uxPHJm5m3RLJn1UrnpcX6VkNNFsCALUmv5xb3Mvb'}
     data = requests.get('https://api.propublica.org/congress/v1/bills/upcoming/house.json', headers=header_data)
     data = data.json()
-
+    myDict = {}
     for item in data[RESULTS][0][BILLS]:
+        billDescription = item[DESCRIPTION]
         billno = item['bill_number']
         googlepage = requests.get("https://www.google.com/search?q="+billno)
         soup = BeautifulSoup(googlepage.content, "lxml")
-        aboutStuff = soup.find(id="resultStats")
-        print(re.search('(?<=About ).*(?= results)', aboutStuff)[0].replace(',', ''))
+        aboutStuff = soup.find(id="resultStats").text
+        associatedNumber = (re.search('(?<=About ).*(?= results)', (aboutStuff)).group(0).replace(',', '')) # also convert to int
+        #fix the billDescription stuff
+        print billDescription 
+        myDict[billDescription] = int(associatedNumber)
+    
+    print sorted(myDict, reverse=True)
+    print "sorted"
 
-
-'''
-Things to do:
-BeautifulSoup stuff
-Sentiment?
-Lucas' API
-host this on heroku/serveo/ngrok
-'''
+    return sorted(myDict, reverse=True)
 
 @app.route("/latlng/<lat>/<lng>")
 def latlng(lat, lng):
@@ -64,3 +66,4 @@ def get_serialized_data(data):
         official_dict['state'] = official['office_details']['district']['state']
         officials_data.append(official_dict)
     return json.dumps(officials_data)
+
